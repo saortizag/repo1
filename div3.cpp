1a09571916644e57f27fb2595034484351bd8ad1
Santiago Ortiz Aguirre <saortizag@unal.edu.co>
2017-02-03 13:02:26 -0500

    programa que divide entre 3

---
+++ div3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main (void)
+{	float numero;
+	cout <<"Escriba un número entero" <<endl; //Lo que el usuario ve
+	cin >>numero; //Lo que el usuario escribe
+	cout <<numero/3.0 <<endl; //Resultado
+return 0;}
