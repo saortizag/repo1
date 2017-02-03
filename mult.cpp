8a3ecf579df16ffbfda745269a80be4c469ae217
Santiago Ortiz Aguirre <saortizag@unal.edu.co>
2017-02-03 12:55:48 -0500

    programa de multiplicación por dos

---
+++ mult.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main (void)
+{	int numero;
+	cout <<"Escriba un número entero" <<endl; //Lo que el usuario ve
+	cin >>numero; //Lo que el usuario escribe
+	cout <<numero*2 <<endl; //Resultado
+return 0;}
