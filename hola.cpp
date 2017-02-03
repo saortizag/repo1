84f429fe1820f09342c5cf72cd4c7a9c4c6c5f33
Santiago Ortiz Aguirre <saortizag@unal.edu.co>
2017-02-03 12:56:19 -0500

    programa que saluda

---
+++ hola.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main (void)
+	{char nombre [30];//[30] es la cantidad de caracteres que el bicho lee.
+	 cout << "Escribe tu nombre; quiero saludar." <<endl; //Lo que le pido (lee) al usuario.
+	 cin >> nombre; //Lo que el usuario mete.
+	 cout << "Hola " <<nombre <<endl; //Hola_nombre
+	 return 0;}
